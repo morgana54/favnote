81f6f7e9f83f3e0526bf712d927e816d9d3cd3ce
Jakub Pietrzak <jpietrzi@gmail.com>
2021-04-28 17:59:55 +0200

    end preparing first atom components

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Button from 'components/atoms/Button';
+import GlobalStyle from 'theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/MainTheme';
 
-function Root() {
-  return <div>e</div>;
-}
+const Root = () => (
+  <div>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <h1>Hello Roman</h1>
+      <Button>Close / Save</Button>
+      <Button secondary>Remove</Button>
+    </ThemeProvider>
+  </div>
+);
 
 export default Root;
